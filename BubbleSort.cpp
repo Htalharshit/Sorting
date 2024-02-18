81ce50adf66755aba883c325f79f4e589265389f
Htalharshit <harshitagarwalhtalhtal@gmail.com>
2024-02-18 13:05:52 +0530

    modified

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -29,9 +29,9 @@ int main()
 {
     int n,i;
     cin>>n;
-    int arr[n];
+    int arra[n];
     for(i=0;i<n;i++)
-        cin>>arr[i];
-    BubbleSort(arr,n);
+        cin>>arra[i];
+    BubbleSort(arra,n);
     return 0;
 }
